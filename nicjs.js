75482e944d3ca0ec4aa78ec07099333a5fc5f528
Nino678190 <115342129+Nino678190@users.noreply.github.com>
2024-05-04 13:08:47 +0200

    js Nic

---
+++ nicjs.js
@@ -0,0 +1,18 @@
+//Datein hochladen -> event listener + fetch
+//Datein runterladen -> fetch
+// Cheatsheet: https://www.freecodecamp.org/news/upload-files-with-javascript/
+// https://razorfxnetworking.medium.com/how-to-set-up-a-simple-http-server-for-local-file-sharing-with-python-36495f116bb9
+//https://dev.to/nelsonmendezz_/how-to-create-server-of-files-with-fastapi-47d0
+//Server: https://razorfxnetworking.medium.com/how-to-set-up-a-simple-http-server-for-local-file-sharing-with-python-36495f116bb9
+
+const url = ''
+
+async function dateiholen(){
+    const antwort = await fetch('')
+    const datein = await response.json()
+    console.log(datein.dateiname)
+}
+
+
+
+
